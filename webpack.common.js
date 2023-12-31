b53cc407c1bdda2f11c7ec59c35941608873887d
PAQUERIAUD Baptiste <baptiste.paqueriaud@etu.univ-amu.fr>
2023-12-31 22:49:53 +0100

    Adds base index.html template

--- webpack.common.js
+++ webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Random sentence picker',
+            template: path.resolve(__dirname, './src/index.html'),
             //favicon: path.resolve(__dirname, './src/images/favicon.ico')
         }),
         new CleanWebpackPlugin(),
